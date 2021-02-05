9f2614394278230422e072978f50ad737e057d5c
wes-public-apps <murraywj@yahoo.com>
2021-02-05 12:01:34 -0800

    handle login post

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 // import express library
 const express= require('express');
 const app=express();
+const bodyParser = require('body-parser'); // for post requests
+
+// register necessary libraries
+app.use(bodyParser.json());
 
 // create mock JSON data
 const mockData = [
@@ -8,6 +12,7 @@ const mockData = [
     {name: "jill"}
 ]
 
+// handle get requests
 app.get('/users',(req,res)=>{
     res.json({
         success: true,
@@ -24,5 +29,27 @@ app.get('/users/:id',(req,res)=>{
         users: req.params.id
     });
 });
+
+//handle post requests
+app.post('/login',(req,res)=>{
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const mockUserName = "username";
+    const mockPassword = "password";
+
+    if(username===mockUserName && password===mockPassword){
+        res.json({
+            success: true,
+            message: "access granted",
+            token: 'cookie'
+        });
+    }else{
+        res.json({
+            success: false,
+            message: "username or password incorrect"
+        });
+    }
+})
 
 app.listen(8000,()=> console.log("server is running"));
