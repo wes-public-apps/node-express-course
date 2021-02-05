a2f5ed11db3562ca70bec75b157c0bb17090b20d
wes-public-apps <murraywj@yahoo.com>
2021-02-05 11:44:42 -0800

    have server return mock data

--- server.js
+++ server.js
@@ -1,4 +1,19 @@
+// import express library
 const express= require('express');
 const app=express();
+
+// create mock JSON data
+const mockData = [
+    {name: "mark"},
+    {name: "jill"}
+]
+
+app.get('/users',(req,res)=>{
+    res.json({
+        success: true,
+        message: 'successfully got users. Nice!',
+        users: mockData
+    });
+});
 
 app.listen(8000,()=> console.log("server is running"));
